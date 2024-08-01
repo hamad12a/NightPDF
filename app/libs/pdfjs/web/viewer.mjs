a0c569ea3f36ebc46c5d1f707e8df93c742da4ca
hamad12a <hamad12a@hotmail.com>
2024-08-01 21:00:34 +0100

    sidebarViewOnLoad: {     value: 1,

--- app/libs/pdfjs/web/viewer.mjs
+++ app/libs/pdfjs/web/viewer.mjs
@@ -4534,7 +4534,7 @@ const defaultOptions = {
     kind: OptionKind.VIEWER
   },
   sidebarViewOnLoad: {
-    value: -1,
+    value: 1,
     kind: OptionKind.VIEWER + OptionKind.PREFERENCE
   },
   scrollModeOnLoad: {
